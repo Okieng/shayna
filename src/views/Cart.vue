15dc226cb561072d9494b848d51b10e2f192d761
Okieng <yogisuk69@gmail.com>
2021-09-23 11:40:08 +0700

    checkout

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -93,6 +93,7 @@
                         id="namaLengkap"
                         aria-describedby="namaHelp"
                         placeholder="Masukan Nama"
+                        v-model="customerInfo.name"
                       />
                     </div>
                     <div class="form-group">
@@ -103,6 +104,7 @@
                         id="emailAddress"
                         aria-describedby="emailHelp"
                         placeholder="Masukan Email"
+                        v-model="customerInfo.email"
                       />
                     </div>
                     <div class="form-group">
@@ -113,6 +115,7 @@
                         id="noHP"
                         aria-describedby="noHPHelp"
                         placeholder="Masukan No. HP"
+                        v-model="customerInfo.number"
                       />
                     </div>
                     <div class="form-group">
@@ -121,6 +124,7 @@
                         class="form-control"
                         id="alamatLengkap"
                         rows="3"
+                        v-model="customerInfo.address"
                       ></textarea>
                     </div>
                   </form>
@@ -132,14 +136,19 @@
             <div class="row">
               <div class="col-lg-12">
                 <div class="proceed-checkout">
-                  <ul>
+                  <ul class="text-left">
                     <li class="subtotal">
                       ID Transaction <span>#SH12000</span>
                     </li>
-                    <li class="subtotal mt-3">Subtotal <span>$240.00</span></li>
-                    <li class="subtotal mt-3">Pajak <span>10%</span></li>
                     <li class="subtotal mt-3">
-                      Total Biaya <span>$440.00</span>
+                      Subtotal <span>${{ totalHarga }}</span>
+                    </li>
+                    <li class="subtotal mt-3">
+                      Pajak <span>10% {{ ditambahPajak }}</span>
+                    </li>
+                    <li class="subtotal mt-3">
+                      Total Biaya
+                      <span>{{ totalBiaya }}</span>
                     </li>
                     <li class="subtotal mt-3">
                       Bank Transfer <span>Mandiri</span>
@@ -151,10 +160,11 @@
                       Nama Penerima <span>Shayna</span>
                     </li>
                   </ul>
-                  <router-link to="/success"
-                    ><a href="#" class="proceed-btn"
-                      >I ALREADY PAID</a
-                    ></router-link
+                  <!--<router-link to="/success"-->
+                  ><a @click="checkout()" href="#" class="proceed-btn"
+                    >I ALREADY PAID</a
+                  >
+                  <!--</router-link-->
                   >
                 </div>
               </div>
@@ -169,7 +179,7 @@
 
 <script>
 import Header from "@/components/Header.vue";
-
+import axios from "axios";
 export default {
   name: "Cart",
   components: {
@@ -178,6 +188,12 @@ export default {
   data() {
     return {
       keranjangUser: [],
+      customerInfo: {
+        name: "",
+        email: "",
+        number: "",
+        address: "",
+      },
     };
   },
   methods: {
@@ -185,6 +201,25 @@ export default {
       this.keranjangUser.splice(index, 1);
       const parsed = JSON.stringify(this.keranjangUser);
       localStorage.setItem("keranjangUser", parsed);
+    },
+    checkout() {
+      let productIds = this.keranjangUser.map(function(product) {
+        return product.id;
+      });
+      let checkoutData = {
+        name: this.customerInfo.name,
+        email: this.customerInfo.email,
+        number: this.customerInfo.number,
+        address: this.customerInfo.address,
+        transaction_total: this.totalBiaya,
+        transaction_status: "PENDING",
+        transaction_details: productIds,
+      };
+
+      axios
+        .post("http://127.0.0.1:8000/api/checkout", checkoutData)
+        .then(() => this.$router.push("success"))
+        .catch((err) => console.log(err));
     },
   },
   mounted() {
@@ -195,6 +230,20 @@ export default {
         localStorage.removeItem("keranjangUser");
       }
     }
+  },
+  computed: {
+    totalHarga() {
+      return this.keranjangUser.reduce(function(items, data) {
+        return items + data.price;
+      }, 0);
+    },
+    ditambahPajak() {
+      return (this.totalHarga * 10) / 100;
+    },
+
+    totalBiaya() {
+      return this.totalHarga + this.ditambahPajak;
+    },
   },
 };
 </script>
